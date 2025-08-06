c734463c02ce63035fbe8c1e1b9d1912b62a89f2
satochi-yamamoto <alextoyama@hotmail.com>
2025-08-06 16:27:06 -0300

    Update ads.txt and index.html for Google AdSense integration; add scripts.js and style.css for site styling and functionality

---
+++ scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  if (!window.dataLayer) {
+    console.warn("Google Analytics não carregado.");
+  } else {
+    console.log("Google Analytics detectado.");
+  }
+});
